754beb5a18a04fa5b4e965b45d9bf1f73af992a6
Aman0802 <aman.khubani@gmail.com>
2020-10-29 21:55:13 +0530

    basic setup of ts config amd basic packages

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    // to create a post, but the below line has nothing to do with the databse
+    const post = orm.em.create(Post, {title: 'My first Post'})
+    
+    //to insert into a database
+    await orm.em.persistAndFlush(post)
+    console.log('---------------sql 2 ------------------')
+    await orm.em.nativeInsert(Post, {title: 'My first post 2'})
+
+}
+
+main().catch(err => console.error(err));
+
+
+console.log("Hello there by Aman")
