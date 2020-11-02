7ff3a8d9299c19cb655929fb3716dcd6df507416
Aman0802 <aman.khubani@gmail.com>
2020-11-02 15:05:52 +0530

    setup for connecting to db, with error

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,15 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
 
     // to create a post, but the below line has nothing to do with the databse
     const post = orm.em.create(Post, {title: 'My first Post'})
     
     //to insert into a database
     await orm.em.persistAndFlush(post)
-    console.log('---------------sql 2 ------------------')
-    await orm.em.nativeInsert(Post, {title: 'My first post 2'})
+    // console.log('---------------sql 2 ------------------')
+    // await orm.em.nativeInsert(Post, {title: 'My first post 2'})
 
 }
 
